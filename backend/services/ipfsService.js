7563d6f127c23fd026c1fbfb9fed4e1c5d27c5ec
Hadi Rezaei <hadirezaei1377hr@gmail.com>
2026-06-27 12:31:21 +0330

    stop ipfs

--- backend/services/ipfsService.js
+++ backend/services/ipfsService.js
@@ -1,30 +1,6 @@
-const { create } = require("ipfs-http-client");
-
-const fs = require("fs");
-
-const client = create({
-
-    url: "http://127.0.0.1:5001/api/v0"
-
-});
-
 const uploadToIPFS = async (filePath) => {
 
-    try {
-
-        const file = fs.readFileSync(filePath);
-
-        const result = await client.add(file);
-
-        return result.cid.toString();
-
-    }
-
-    catch (error) {
-
-        throw error;
-
-    }
+    return "QmMockCID123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
 };
 
